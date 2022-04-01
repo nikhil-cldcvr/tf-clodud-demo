a8a72d6160d436d03e42f71b5588d94b765fc52a
Nikhil Pandit <73530503+nikhil-cldcvr@users.noreply.github.com>
2022-04-01 08:33:04 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,6 +2,13 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
+    network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
